63756420c5ef0970e9a26ea477939af1c234d540
Matthew Esparza <matthewesparza12@gmail.com>
2019-09-05 15:12:22 -0500

    Updated with regions

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -11,13 +11,21 @@ class Pokemon extends Component {
             url: "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151",
             pokemon: '',
             loading: false,
-            search: ''
+            search: '',
+            region: 'Kanto'
         }   
         this.filterPokemon = this.filterPokemon.bind(this);
+        this.kanto = this.kanto.bind(this);
+        this.johto = this.johto.bind(this);
+        this.hoenn = this.hoenn.bind(this);
+        this.sinnoh = this.sinnoh.bind(this);
     }
 
-    componentWillUpdate() {
-        console.log(this.state.search);
+    async componentWillUpdate() {
+        const res = await axios.get(this.state.url);
+        this.setState({ 
+            pokemon: res.data['results']
+        });
     }
     async componentDidMount() {
         const res = await axios.get(this.state.url);
@@ -27,11 +35,41 @@ class Pokemon extends Component {
         
       }
     
+    
     filterPokemon(event) {
         this.setState({
             search: event.target.value,
         });
     }
+
+    kanto() {
+        this.setState({
+            region: "Kanto",
+            url: "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151",
+        })
+    }
+
+    johto() {
+        this.setState({
+            region: "Johto",
+            url: "https://pokeapi.co/api/v2/pokemon/?offset=151&limit=100",
+        })
+    }
+    
+    hoenn() {
+        this.setState({
+            region: "Hoenn",
+            url: "https://pokeapi.co/api/v2/pokemon/?offset=251&limit=135",
+        })
+    }
+
+    sinnoh() {
+        this.setState({
+            region: "Sinnoh",
+            url: "https://pokeapi.co/api/v2/pokemon/?offset=386&limit=107",
+        })
+    }
+
 
 
     render() {
@@ -53,10 +91,13 @@ class Pokemon extends Component {
             <div>
 
                 <Navbar className="widthfix" sticky="top" bg="danger" variant="dark">
-                <Navbar.Brand href="#home">Kanto PokeAPI</Navbar.Brand>
+                <Navbar.Brand href="#home">{this.state.region} PokeAPI</Navbar.Brand>
                     <Form inline>
                     <FormControl type="text" placeholder="Search" value={this.state.search} onChange={this.filterPokemon} className="mr-sm-2" />
-                    <h3>{searching}</h3>
+                    <Button className="rpad" varient="secondary" onClick={this.kanto}>Kanto</Button>
+                    <Button className="rpad" varient="secondary" onClick={this.johto}>Johto</Button>
+                    <Button className="rpad" varient="secondary" onClick={this.hoenn}>Hoenn</Button>
+                    <Button className="rpad" varient="secondary" onClick={this.sinnoh}>Sinnoh</Button>
                     </Form>
                 </Navbar>
             {loader}
